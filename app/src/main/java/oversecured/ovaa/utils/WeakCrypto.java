d138b1f4aa7ff8eddd7b22b4aec502e701b89bbb
jackrong1104 <jrong@cismobile.com>
2022-08-09 19:16:38 -0400

    Fixed AES key

--- app/src/main/java/oversecured/ovaa/utils/WeakCrypto.java
+++ app/src/main/java/oversecured/ovaa/utils/WeakCrypto.java
@@ -6,14 +6,16 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
 public class WeakCrypto {
-    private static final String KEY = "49u5gh249gh24985ghf429gh4ch8f23f";
 
     private WeakCrypto() {
     }
 
+    private String getData() {
+       return "49u5gh249gh24985ghf429gh4ch8f23f";
+    }
     public static String encrypt(String data) {
         try {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(KEY.getBytes(), "AES");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(getData().getBytes(), "AES");
             Cipher instance = Cipher.getInstance("AES");
             instance.init(Cipher.ENCRYPT_MODE, secretKeySpec);
             return Base64.encodeToString(instance.doFinal(data.getBytes()), 0);
