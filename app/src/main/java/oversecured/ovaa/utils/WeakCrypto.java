f115ef0aab323e8c29119bbd880c5fd42897e9fd
jackrong1104 <jrong@cismobile.com>
2022-08-09 19:18:32 -0400

    Fixed static call

--- app/src/main/java/oversecured/ovaa/utils/WeakCrypto.java
+++ app/src/main/java/oversecured/ovaa/utils/WeakCrypto.java
@@ -10,7 +10,7 @@ public class WeakCrypto {
     private WeakCrypto() {
     }
 
-    private String getData() {
+    private static String getData() {
        return "49u5gh249gh24985ghf429gh4ch8f23f";
     }
     public static String encrypt(String data) {
