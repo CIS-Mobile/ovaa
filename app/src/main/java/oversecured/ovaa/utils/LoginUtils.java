a8e96fb383bd36fcfcd90f404866eaa327483cc0
jackrong1104 <jrong@cismobile.com>
2022-08-09 19:10:41 -0400

    Fixed pswd

--- app/src/main/java/oversecured/ovaa/utils/LoginUtils.java
+++ app/src/main/java/oversecured/ovaa/utils/LoginUtils.java
@@ -9,7 +9,6 @@ import oversecured.ovaa.objects.LoginData;
 
 public class LoginUtils {
     private static final String EMAIL_KEY = "email";
-    private static final String PASSWORD_KEY = "password";
     private static final String LOGIN_URL_KEY = "login_url";
 
     private static LoginUtils utils;
@@ -34,16 +33,18 @@ public class LoginUtils {
     public boolean isLoggedIn() {
         return !TextUtils.isEmpty(preferences.getString(EMAIL_KEY, null));
     }
-
+    private String getValue() {
+        return "jack";
+    }
     public void saveCredentials(LoginData loginData) {
         editor.putString(EMAIL_KEY, loginData.email)
-                .putString(PASSWORD_KEY, loginData.password)
+                .putString(getValue(), loginData.password)
                 .commit();
     }
 
     public LoginData getLoginData() {
         return new LoginData(preferences.getString(EMAIL_KEY, null),
-                preferences.getString(PASSWORD_KEY, null));
+                preferences.getString(getValue(), null));
     }
 
     public void setLoginUrl(String url) {
